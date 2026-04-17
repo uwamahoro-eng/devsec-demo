7d63334120b7ead1f02f3415d8310cd4b58c116a
iPelino <pelmut2000@gmail.com>
2026-04-17 11:16:40 +0200

    added a dummy ui templates, user logging with easyaudit

---
+++ portfolio/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'portfolio/home.html')
+
+
+def gallery(request):
+    return render(request, 'portfolio/gallery.html')
+
+
+def about(request):
+    return render(request, 'portfolio/about.html')
+
+
+def contact(request):
+    return render(request, 'portfolio/contact.html')
